b83c4706c80d9d93f4c7ae63068e1c352924a3b5
Tom Myers <tom.stephen.myers@gmail.com>
2014-05-20 15:13:06 +0100

    Working app engine version.

--- main.go
+++ main.go
@@ -1,20 +1,24 @@
-package main
+package prchecker
 
 import (
 	"appengine"
+	"appengine/urlfetch"
 	"encoding/json"
 	"fmt"
-	"github.com/strava/go.strava"
+	"github.com/Ferguzz/go.strava"
+	"github.com/gorilla/mux"
+	"github.com/gorilla/securecookie"
+	"github.com/gorilla/sessions"
 	"html/template"
 	"io/ioutil"
+	"log"
 	"net/http"
-	"os"
 	"strconv"
+	"time"
 )
 
-var PORT int = 8080
-var accessToken string
-var athlete strava.AthleteDetailed
+var store = sessions.NewCookieStore(securecookie.GenerateRandomKey(64))
+var sessionName = "prchecker"
 var templates = template.Must(template.ParseFiles("html/input.html", "html/results.html"))
 
 type SegmentInfo struct {
@@ -25,6 +29,7 @@ type SegmentInfo struct {
 }
 
 type AppConfig struct {
+	Domain       string
 	ClientID     int
 	ClientSecret string
 }
@@ -33,92 +38,113 @@ func init() {
 	configFile := "appconfig.json"
 	configData, err := ioutil.ReadFile(configFile)
 	if err != nil {
-		fmt.Printf("Can't open configuration file: %s\n", configFile)
-		os.Exit(1)
+		log.Fatal("Can't open configuration file: %s\n", configFile)
 	}
 
 	var config AppConfig
 	err = json.Unmarshal(configData, &config)
 	if err != nil {
-		fmt.Println("Can't parse configuration file.")
-		os.Exit(1)
+		log.Fatal("Can't parse configuration file.\n")
 	}
 
 	strava.ClientId = config.ClientID
 	strava.ClientSecret = config.ClientSecret
 
-	http.HandleFunc("/", homeHandler)
-	http.HandleFunc("/results/", resultsHandler)
+	strava.OAuthCallbackURL = fmt.Sprintf("%s/auth/", config.Domain)
+	path, err := strava.OAuthCallbackPath()
+	if err != nil {
+		log.Fatal("Can't set authorization callback URL: %s\n", err)
+	}
+
+	r := mux.NewRouter()
+	r.HandleFunc(path, strava.OAuthCallbackHandler(authSuccess, authFailure))
+	r.HandleFunc("/", homeHandler)
+	r.HandleFunc("/results/", resultsHandler)
+	http.Handle("/", r)
 }
 
 func homeHandler(w http.ResponseWriter, r *http.Request) {
-	checkAuth(w, r)
-	renderTemplate("input.html", w, nil)
+	session, _ := store.Get(r, sessionName)
+	if !checkAuth(session) {
+		authHandler(w, r)
+	} else {
+		renderTemplate("input.html", w, nil)
+	}
 }
 
 func resultsHandler(w http.ResponseWriter, r *http.Request) {
-	checkAuth(w, r)
-
-	c := appengine.NewContext(r)
-	client := strava.NewClient(accessToken)
-
-	activityId, err := strconv.ParseInt(r.FormValue("activity_id"), 0, 64)
-	if err != nil {
-		// Retrieve last activity
-		athletesService := strava.NewAthletesService(client)
-		activities, err := athletesService.ListActivities(athlete.Id).Page(1).PerPage(1).Do()
-		if err != nil {
-			c.Infof("Can't get requested activity: %s", err)
-			os.Exit(1)
+	session, _ := store.Get(r, sessionName)
+	if !checkAuth(session) {
+		authHandler(w, r)
+	} else {
+		c := appengine.NewContext(r)
+		tr := urlfetch.Transport{
+			Context:                       c,
+			Deadline:                      60 * time.Second,
+			AllowInvalidServerCertificate: false,
 		}
-		activityId = activities[0].Id
-	}
 
-	activitiesService := strava.NewActivitiesService(client)
-	detail, err := activitiesService.Get(activityId).IncludeAllEfforts().Do()
-	if err != nil {
-		c.Infof("Can't get segment efforts for requested activity: %s", err)
-		os.Exit(1)
-	}
+		accessToken, _ := session.Values["accessToken"].(string)
+		athleteId, _ := session.Values["athleteId"].(int64)
+		client := strava.NewClient(accessToken, &http.Client{Transport: &tr})
 
-	data := make([]SegmentInfo, len(detail.SegmentEfforts))
-	segmentService := strava.NewSegmentsService(client)
-
-	for i, effort := range detail.SegmentEfforts {
-		elapsedTime := effort.ElapsedTime
-		segment, err := segmentService.Get(int(effort.Segment.Id)).Do()
+		activityId, err := strconv.ParseInt(r.FormValue("activity_id"), 0, 64)
 		if err != nil {
-			c.Infof("Can't get segment detail for requested activity: %s", err)
-			os.Exit(1)
+			// Retrieve last activity
+			athletesService := strava.NewAthletesService(client)
+			activities, err := athletesService.ListActivities(athleteId).Page(1).PerPage(1).Do()
+			if err != nil {
+				http.Error(w, fmt.Sprintf("Can't get requested activity: %s", err), http.StatusInternalServerError)
+			}
+			activityId = activities[0].Id
 		}
-		prTime := segment.PRTime
 
-		data[i].Name = effort.Name
-		data[i].PRTime = prTime
-		data[i].ElapsedTime = elapsedTime
-		data[i].Percentage = int(float32(elapsedTime) / float32(prTime) * 100)
+		activitiesService := strava.NewActivitiesService(client)
+		detail, err := activitiesService.Get(activityId).IncludeAllEfforts().Do()
+		if err != nil {
+			http.Error(w, fmt.Sprintf("Can't get segment efforts for requested activity: %s", err), http.StatusInternalServerError)
+		}
+
+		data := make([]SegmentInfo, len(detail.SegmentEfforts))
+		segmentService := strava.NewSegmentsService(client)
+
+		for i, effort := range detail.SegmentEfforts {
+			elapsedTime := effort.ElapsedTime
+			segment, err := segmentService.Get(int(effort.Segment.Id)).Do()
+			if err != nil {
+				http.Error(w, fmt.Sprintf("Can't get segment detail for requested activity: %s", err), http.StatusInternalServerError)
+			}
+			prTime := segment.PRTime
+
+			data[i].Name = effort.Name
+			data[i].PRTime = prTime
+			data[i].ElapsedTime = elapsedTime
+			data[i].Percentage = int(float32(elapsedTime) / float32(prTime) * 100)
+		}
+		renderTemplate("results.html", w, data)
 	}
-
-	renderTemplate("results.html", w, data)
 }
 
-func checkAuth(w http.ResponseWriter, r *http.Request) {
-	if accessToken == "" {
-		c := appengine.NewContext(r)
-		strava.OAuthCallbackURL = fmt.Sprintf("http://localhost:%d/auth/", PORT)
-		path, err := strava.OAuthCallbackPath()
-		if err != nil {
-			c.Infof("Can't set authorization callback URL.\n%s\n", err)
-			os.Exit(1)
-		}
-		http.HandleFunc(path, strava.OAuthCallbackHandler(authSuccess, authFailure))
-		http.Redirect(w, r, strava.OAuthAuthorizationURL("", strava.Permissions.Public, false), http.StatusFound)
+func checkAuth(session *sessions.Session) bool {
+	if v, ok := session.Values["authenticated"]; ok && v == true {
+		return true
+	} else {
+		return false
 	}
+}
+
+func authHandler(w http.ResponseWriter, r *http.Request) {
+	http.Redirect(w, r, strava.OAuthAuthorizationURL("prchecker", strava.Permissions.Public, false), http.StatusFound)
 }
 
 func authSuccess(auth *strava.AuthorizationResponse, w http.ResponseWriter, r *http.Request) {
-	accessToken = auth.AccessToken
-	athlete = auth.Athlete
+	session, _ := store.Get(r, sessionName)
+	session.Values["authenticated"] = true
+	session.Values["accessToken"] = auth.AccessToken
+	session.Values["athleteId"] = auth.Athlete.Id
+	if err := session.Save(r, w); err != nil {
+		http.Error(w, fmt.Sprintf("Couldn't save session: %s\n", err), http.StatusInternalServerError)
+	}
 	http.Redirect(w, r, "/", http.StatusFound)
 }
 
